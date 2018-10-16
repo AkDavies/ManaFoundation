86b0626a3d0e8205249362141d98791edd9401f7
AkDavies <aod@andrew.cmu.edu>
2018-10-15 22:52:58 -0700

    Wrote initial tests for Deck class.

--- test/test_classes.py
+++ test/test_classes.py
@@ -1,5 +1,6 @@
 import unittest
 from src.Card import Card
+from src.Deck import Deck
 
 class TestCard(unittest.TestCase):
     def setUp(self):
@@ -21,11 +22,41 @@ class TestCard(unittest.TestCase):
 
 
 class TestDeck(unittest.TestCase):
+    def setUp(self):
+        self.example_card_1 = Card(name="Plains")
+        self.example_card_2 = Card(name="Island")
+        self.example_card_3 = Card(name="Swamp")
+        self.example_card_4 = Card(name="Mountain")
+        self.example_card_5 = Card(name="Forest")
+        self.example_deck = Deck([self.example_card_1,self.example_card_2, self.example_card_3,
+                                    self.example_card_4, self.example_card_5
+                                ]
+                            )
     def test_shuffle(self):
-        pass
+        self.example_deck.shuffle()
+        drawn_card = self.example_deck.draw()
+        self.assertIsNot(drawn_card, self.example_card_1)
     
     def test_draw(self):
-        pass
+        initial_size = self.example_deck.size
+        drawn_card = self.example_deck.draw()
+        self.assertIsInstance(drawn_card, Card)
+        self.assertEqual(self.example_deck.size, initial_size - 1)
+    
+    def test__add_card(self):
+        another_card = Card(name="Wasteland")
+        self.assertNotIn(another_card, self.example_deck)
+        self.example_deck._add_card(another_card)
+        self.assertIn(another_card, self.example_deck)
+    
+    def test__add_cards(self):
+        another_card = Card(name="Wasteland")
+        another_card_2 = Card(name="Strip Mine")
+        self.assertNotIn(another_card, self.example_deck)
+        self.assertNotIn(another_card_2, self.example_deck)
+        self.example_deck._add_cards([another_card,another_card_2])
+        self.assertIn(another_card, self.example_deck)
+        self.assertIn(another_card_2, self.example_deck)
 
 class TestHand(unittest.TestCase):
     pass
