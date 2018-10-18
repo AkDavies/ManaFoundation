66268ccfaf8673d642606359f8425a468e15d2ac
AkDavies <aod@andrew.cmu.edu>
2018-10-17 23:21:33 -0700

    Added tests for Hand class and got tests passed.

--- src/Hand.py
+++ src/Hand.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
-
+import queue 
 class Hand:
-    def __init__(self):
+
+
+    def __init__(self, cards=None):
         self.cards = []
-        self.playable_lands = None
+        self.playable_land_queue = queue.PriorityQueue()
+        for card in cards:
+            self.add_card(card)
+
+    def __contains__(self, item):
+        return item in self.cards
+
+    def __repr__(self):
+        return self.cards.__repr__()
+
+    @property
+    def size(self):
+        return len(self.cards)
 
     def draw(self, card):
         pass
+    
+    def add_card(self, card):
+        self.cards.append(card)
+        if card.is_land():
+            item = (self.land_priority_strategy(card),card)
+            self.playable_land_queue.put(item)
 
     def play_land(self, ):
-        pass
+        if self.has_playable_land():
+            _, land = self.playable_land_queue.get()
+            self.cards.remove(land)
+            return land
 
+    @staticmethod
+    def land_priority_strategy(card):
+        return 1
     def has_playable_land(self, ):
-        pass
+        return not self.playable_land_queue.empty()
 
-    def land_priority_strategy(self, card):
-        pass
+
 
--- test/test_classes.py
+++ test/test_classes.py
@@ -1,6 +1,7 @@
 import unittest
 from src.Card import Card
 from src.Deck import Deck
+from src.Hand import Hand
 
 class TestCard(unittest.TestCase):
     def setUp(self):
@@ -22,6 +23,10 @@ class TestCard(unittest.TestCase):
         self.example_card.untap()
         self.assertTrue(self.example_card.is_untapped())
     
+    def test_is_land(self):
+        self.assertTrue(self.example_card.is_land())
+        self.assertTrue(Card("Island").is_land())
+
     def test_is_in_play(self):
         pass
     
@@ -67,16 +72,39 @@ class TestDeck(unittest.TestCase):
 class TestHand(unittest.TestCase):
     
     def setUp(self):
-        pass
+        self.cards_in_hand = [Card(name="Island"),Card(name="Island"),Card(name="Swamp"), Card("Cryptic Command")]
+        self.example_hand = Hand(cards=self.cards_in_hand)
+        self.initial_hand_size = self.example_hand.size
+    
+    def test_size(self):
+        self.assertEqual(self.example_hand.size, len(self.cards_in_hand))
     
     def test_draw(self):
         pass
     
+    def test_add_card(self):
+        another_forest = Card(name="Forest")
+        self.assertEqual(self.example_hand.size, self.initial_hand_size)
+        self.example_hand.add_card(another_forest)
+        self.assertEqual(self.example_hand.size, self.initial_hand_size + 1)
+        self.assertIn(another_forest, self.example_hand)
+    
     def test_play_land(self):
-        pass
+        output_card = self.example_hand.play_land()
+        self.assertIsInstance(output_card, Card)
+        self.assertTrue(output_card.is_land())
+        self.assertEqual(self.example_hand.size, self.initial_hand_size - 1)
 
     def test_has_playable_land(self):
-        pass
+        #Example hand starts off with three playable lands, so 
+        #after three land plays there should be no more playable lands
+        self.assertTrue(self.example_hand.has_playable_land())
+        self.example_hand.play_land()
+        self.assertTrue(self.example_hand.has_playable_land())
+        self.example_hand.play_land()
+        self.assertTrue(self.example_hand.has_playable_land())
+        self.example_hand.play_land()
+        self.assertFalse(self.example_hand.has_playable_land())
     
     def test_land_priority_strategy(self):
         pass
@@ -88,4 +116,4 @@ class TestGame(unittest.TestCase):
     pass
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=1)
